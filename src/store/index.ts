fe6c7d1c9a78e31cd2796363aecc80bdeb30f420
Molly J. Whitener <mollyjwhitener@WWPROGRAMMING.COM>
2021-04-09 12:47:31 -0500

    initial commit, working on axios API calls and general logic and setup

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,38 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    quotes: []
+  },
+  getters: {
+    getAllQuotes: state => {
+      return state.quotes
+    },
+    getNumQuotes: num => {
+     return axios.get(`https://ron-swanson-quotes.herokuapp.com/v2/${num}`)
+      .then(response => response.data)
+    },
+    getTermQuotes: term => {
+      return axios.get(`https://ron-swanson-quotes.herokuapp.com/v2/search/${term}`)
+      .then(response => response.data)
+    }
   },
   mutations: {
+    getQuotes(state, quotes) {
+      state.quotes = quotes
+    }
   },
   actions: {
+    getQuotes({commit}) {
+      axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes/109')
+      .then(response => {
+        commit('getQuotes', response.data) 
+      })
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <p>{{allQuotes}}</p>
   </div>
 </template>
 
 <script lang="ts">
+import store from '@/store';
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default Vue.extend({
   name: 'Home',
-  components: {
-    HelloWorld,
+  
+  computed: {
+    allQuotes() {
+      return store.getters.getAllQuotes
+    }
   },
+  mounted() {
+    store.dispatch('getQuotes')
+  }
 });
 </script>
